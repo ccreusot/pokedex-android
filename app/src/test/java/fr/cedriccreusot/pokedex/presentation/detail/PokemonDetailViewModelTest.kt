09a75806d884498bc9c7f7ee57ca7436b46dd690
Jean-Christophe Melikian <jean.melikian@gmail.com>
2021-05-17 18:59:05 +0200

    Refacto PokemonDetailViewModelTest

--- app/src/test/java/fr/cedriccreusot/pokedex/presentation/detail/PokemonDetailViewModelTest.kt
+++ app/src/test/java/fr/cedriccreusot/pokedex/presentation/detail/PokemonDetailViewModelTest.kt
@@ -23,61 +23,71 @@ class PokemonDetailViewModelTest : ViewModelTest() {
 
     private lateinit var useCase: FetchPokemonDetailUseCase
     private lateinit var viewModel: PokemonDetailViewModel
+    private lateinit var observer: Observer<State>
+
+    companion object {
+        private const val NOT_FOUND_ERROR_ARG = 90000
+        private const val NOT_FOUND_ERROR_MESSAGE = "Pokemon[90000] not found"
+        private const val INVALID_ARGUMENT_ERROR_ARG = -1
+        private const val INVALID_ARGUMENT_ERROR_MESSAGE =
+            "Invalid argument '-1' can not be processed"
+        private const val SUCCESSFUL_ARG = 42
+    }
 
     @Before
     override fun setUp() {
         super.setUp()
         useCase = mockk()
         viewModel = PokemonDetailViewModel(useCase)
+        observer = mockk(relaxed = true)
     }
 
     @Test
     fun `when we observe a pokemon details and the usecase returns a NotFoundError - then we should return an error state`() {
         // GIVEN
-        every { useCase(90000) } returns NotFoundError(90000)
-        val observe = mockk<Observer<State>>(relaxed = true)
+        every { useCase(NOT_FOUND_ERROR_ARG) } returns NotFoundError(NOT_FOUND_ERROR_ARG)
 
         // WHEN
-        viewModel.pokemonDetailState().observeForever(observe)
-        viewModel.fetchPokemonDetail(90000)
+        viewModel.pokemonDetailState().observeForever(observer)
+        viewModel.fetchPokemonDetail(NOT_FOUND_ERROR_ARG)
 
         // THEN
-        verify { observe.onChanged(State.Loading) }
-        verify { observe.onChanged(State.Error("Pokemon[90000] not found")) }
-        confirmVerified(observe)
+        verify { observer.onChanged(State.Loading) }
+        verify { observer.onChanged(State.Error(NOT_FOUND_ERROR_MESSAGE)) }
+        confirmVerified(observer)
     }
 
     @Test
     fun `when we observe a pokemon details and the usecase returns a InvalidArgumentError - then we should return an error state`() {
         // GIVEN
-        every { useCase(-1) } returns InvalidArgumentError(-1)
-        val observe = mockk<Observer<State>>(relaxed = true)
+        every { useCase(INVALID_ARGUMENT_ERROR_ARG) } returns InvalidArgumentError(
+            INVALID_ARGUMENT_ERROR_ARG
+        )
 
         // WHEN
-        viewModel.pokemonDetailState().observeForever(observe)
-        viewModel.fetchPokemonDetail(-1)
+        viewModel.pokemonDetailState().observeForever(observer)
+        viewModel.fetchPokemonDetail(INVALID_ARGUMENT_ERROR_ARG)
 
         // THEN
-        verify { observe.onChanged(State.Loading) }
-        verify { observe.onChanged(State.Error("Invalid argument '-1' can not be processed")) }
-        confirmVerified(observe)
+        verify { observer.onChanged(State.Loading) }
+        verify { observer.onChanged(State.Error(INVALID_ARGUMENT_ERROR_MESSAGE)) }
+        confirmVerified(observer)
     }
 
     @Test
     fun `when we observe a pokemon details and the usecase returns a PokemonDetail - then we should return an success state`() {
         // GIVEN
-        val mockedPokemon = mockk<PokemonDetail>(relaxed = true).copy(id = 42)
-        every { useCase(42) } returns Success(mockedPokemon)
-        val observe = mockk<Observer<State>>(relaxed = true)
+        val mockedPokemon = mockk<PokemonDetail>(relaxed = true).copy(id = SUCCESSFUL_ARG)
+        every { useCase(SUCCESSFUL_ARG) } returns Success(mockedPokemon)
 
         // WHEN
-        viewModel.pokemonDetailState().observeForever(observe)
-        viewModel.fetchPokemonDetail(42)
+        viewModel.pokemonDetailState().observeForever(observer)
+        viewModel.fetchPokemonDetail(SUCCESSFUL_ARG)
 
         // THEN
-        verify { observe.onChanged(State.Loading) }
-        verify { observe.onChanged(State.Success(mockedPokemon)) }
-        confirmVerified(observe)
+        verify { observer.onChanged(State.Loading) }
+        verify { observer.onChanged(State.Success(mockedPokemon)) }
+        confirmVerified(observer)
     }
 
 
